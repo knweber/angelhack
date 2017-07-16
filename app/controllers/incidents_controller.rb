e7a8eac83b1d742a1c5f21976687da5a2847e564
Kris <kris.weber.1994@gmail.com>
2017-07-16 11:08:59 -0700

    add ajax and refactor idex route

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -4,16 +4,12 @@ class IncidentsController < ApplicationController
 
   def index
     @incidents = Incident.all
-    # @incidents.each do |incident|
-    #
-    #   coord_pair = '{'+ incident.lat + ', ' + incident.lon + '}'
-    #
-    #   date = incident.month + ' ' + incident.day.to_s + ', ' + incident.year.to_s;
-    #
-    #   info = {:loc => coord_pair, :name =>  incident.name, :date => date, :race => incident.race, :sex => incident.sex, :age => incident.age.to_s, :cause => incident.cause}
-    #
-    #   info.to_json
-    # end
+    add_marker_data = []
+    @incidents.each do |incident|
+      info = incident.single_incident_info
+      add_marker_data.push(info)
+    end
+    add_marker_data.to_json
   end
 
 end
