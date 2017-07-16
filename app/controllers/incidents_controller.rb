98fa91065cba626216ad331a20d56ca179581e8e
Kris <kris.weber.1994@gmail.com>
2017-07-16 13:55:13 -0700

    fix locally

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -3,13 +3,14 @@ require 'json'
 class IncidentsController < ApplicationController
 
   def index
+
     @incidents = Incident.all
-    add_marker_data = []
-    @incidents.each do |incident|
-      info = incident.single_incident_info
-      add_marker_data.push(info)
-    end
-    add_marker_data.to_json
+    # add_marker_data = []
+    # @incidents.each do |incident|
+    #   info = incident.single_incident_info
+    #   add_marker_data.push(info)
+    # end
+    # add_marker_data.to_json
   end
 
 end
